99d99f83db8a06e224236832b1e1ea8d086ad426
Oleh Didushok <didushokov@gmail.com>
2025-05-05 16:48:27 +0300

    Created app

---
+++ MusicSynth/MusicSynth/MusicSynthApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicSynthApp.swift
+//  MusicSynth
+//
+//  Created by Oleh on 05.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct MusicSynthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
